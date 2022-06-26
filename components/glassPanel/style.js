c8ff75e12e326dc838fc7115bdbe1d7614564aad
Anirban Aditya Halder <nabovtl@gmail.com>
2022-06-26 19:52:09 +0530

    pure css

--- components/glassPanel/style.js
+++ components/glassPanel/style.js
@@ -9,12 +9,7 @@ import Image from "next/image"
 
 export const Container = styled.div`
 
-    // transform : scale(0.95);
-    
-    // margin : 0.2em;
-    // font-size : 1em;
     padding : 0.5em;
-    // padding-left:0.8em;
 
     background-color: rgba(255, 0, 102, 0.17);
 
@@ -22,22 +17,8 @@ export const Container = styled.div`
     min-height : 7em;
 
 
-    // box-shadow : 0 0 2em rgba(236, 72, 153, 0.2), inset 0 0 4em rgba(255,255,255, 0.05);
-
-
-
-    // backdrop-filter: saturate(200%);
-    // backdrop-filter: brightness(60%);
     backdrop-filter: blur(5px);
-
-    // -webkit-backdrop-filter: saturate(200%);
-    // -webkit-backdrop-filter: brightness(60%);
     -webkit-backdrop-filter: blur(5px);
-
-    // transition : all 0.25s ease-in;
-    // &:hover {
-    //     transform : scale(1);
-    // }
 
 `
 
--- components/portfolio/portfolio.js
+++ components/portfolio/portfolio.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react"
 import GlassPanel from "../glassPanel/glassPanel"
 import { Button } from "@mui/material"
 
-import { Container } from "../glassPanel/style"
+// import { div } from "../glassPanel/style"
 
 import Script from 'next/script'
 
@@ -73,43 +73,43 @@ export default function Portfolio(props){
                     <div className="textAnim prnt mt-[0em] p-1 rounded-[0.5em] grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-2 ">
 
                         <a target = "_blank" href="http://tidbits.rf.gd" className="prnt2">
-                          <Container className="chld" >
+                          <div className="chld" >
                             <div className="text-2xl md:text-[1em]">Tidbits</div>
                             <div className="fun text-2xl md:text-[0.625em] m-3">This unholy mess of <Pink>PHP, HTML,CSS and SQL</Pink> was the first site I developed. Just an ordinary blogging website with an extraordinarily confusing backend in Vanilla PHP.</div>
                             {/* <div className="fun text-2xl md:text-[0.625em] m-3"><Pink >link</Pink></div>
                             <div className="fun text-2xl md:text-[0.625em] m-3"><Pink >link</Pink></div> */}
-                          </Container >
+                          </div >
                         </a>
 
                         <a target = "_blank" href="https://github.com/banboy38/IEEE_Mega_Project" className="prnt2">
-                          <Container className="chld" >
+                          <div className="chld" >
                             <div className="text-2xl md:text-[1em]">CollegeDB</div>
                             <div className="fun text-2xl md:text-[0.625em] m-3">A simple content management site for <Pink>BIT Mesra batch of 23'</Pink>. The authentication part is <Pink>significantly stronger</Pink> than the previous project, apart from other <Pink>improvements</Pink> in backend. Same stack though.</div>
-                          </Container>
+                          </div>
                         </a>
                         <a target = "_blank" href="https://banboy38.github.io/" className="prnt2">
-                          <Container className="chld" >
+                          <div className="chld" >
                             <div className="text-2xl md:text-[1em]">Previous Portfolio</div>
                             <div className="fun text-2xl md:text-[0.625em] m-3">Here's my previous portfolio site from 3 years ago when I wasn't the <Pink>God of Pessimism</Pink> yet. So naturally, it's complete and utter shit.</div>
-                          </Container>
+                          </div>
                         </a>
                         <a target = "_blank" href="https://hackaduck.anirbanhalder.social/" className="prnt2">
-                          <Container className="chld" >
+                          <div className="chld" >
                             <div className="text-2xl md:text-[1em]">Hack-A-Duck 2.0</div>
                             <div className="fun text-2xl md:text-[0.625em] m-3">Developed the website for <Pink>Hack-A-Duck</Pink>, the premier hackathon organized by <Pink>Psyberduck in collaboration with IET, BIT Mesra.</Pink> It was an attempt to make something unique, wacky and aesthetic. Glad it was a success 😎.</div>
-                          </Container>
+                          </div>
                         </a>
                         <a target = "_blank" href="https://devsdungeon.anirbanhalder.social/" className="prnt2">
-                          <Container className="chld" >
+                          <div className="chld" >
                             <div className="text-2xl md:text-[1em]">Devs Dungeon</div>
                             <div className="fun text-2xl md:text-[0.625em] m-3">Developed the community website for <Pink>DevsDungeon.</Pink> Was already provided a design template for this one and couldn't improvise much, so it's quite <Pink>drab.</Pink></div>
-                          </Container>
+                          </div>
                         </a>
                         <a href="/#" className="prnt2">
-                          <Container className="chld" >
+                          <div className="chld" >
                             <div className="text-2xl md:text-[1em]">This Site</div>
                             <div className="fun text-2xl md:text-[0.625em] m-3">I started off with a React project, then later ended up making a <Pink>NextJs App along with TailwindCSS and Styled Components.</Pink><br/><Pink>Ggwp.</Pink></div>
-                          </Container>
+                          </div>
                         </a>
 
                     </div>                      
