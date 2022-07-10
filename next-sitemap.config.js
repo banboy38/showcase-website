fe6828a0924171b79567719a17eb458a5124b20a
Anirban Aditya Halder <nabovtl@gmail.com>
2022-07-10 14:16:36 +0530

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,21 +1,18 @@
-/** @type {import('next-sitemap').IConfig} */
+const siteUrl = "https://anirbanhalder.social/";
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
 
-const config = {
-    siteUrl: process.env.SITE_URL || 'https://www.anirbanhalder.social',
-    generateRobotsTxt: true,
-    robotsTxtOptions: {
-      policies: [
-        {
-          userAgent: "*",
-          allow: "/",
-        },
-      ],
-  
-      additionalPaths: async (config) => [
-        await config.transform(config, "/comments"),
-      ],
-     
-    },
-  }
-  
-  export default config
+    additionalPaths: async (config) => [
+      await config.transform(config, "/comments"),
+    ],
+   
+  },
+};
